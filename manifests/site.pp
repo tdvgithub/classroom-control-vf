1829b5a31b0e7b941d0365bdd9826bd79cb3d8ec
tdvgithub <rq.itnerd@gmail.com>
2016-05-24 09:56:27 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,4 +43,10 @@ node default {
   # Example:
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
+  file { '/etc/motd':
+  ensure  => file,
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  content => "Think before you type\n",
 }
