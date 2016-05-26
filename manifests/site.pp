645ab78960eb10ded6950b05ccf6a4c9b1d038b7
tdvgithub <rq.itnerd@gmail.com>
2016-05-26 09:50:51 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,8 +42,6 @@ node default {
 # This is where you can declare classes for all nodes.
 # Example:
 # class { 'my_class': }
-if $::virtual != 'physical' {
-$vmname = capitalize($::virtual)
-notify { "This is a ${vmname} virtual machine.": }
-}
+$message = hiera('message')
+notify { $message: }
 }
